f9573adacef72845593c8a06b6e6df47da3a58ef
dskjal <16787332+dskjal@users.noreply.github.com>
2025-10-05 08:40:10 +0900

    Add files via upload

--- run.py
+++ run.py
@@ -2,21 +2,23 @@ import gradio as gr
 from utils import App
 import glob
 
+DEFAULT_LLM = "./models/Anime-Llasa-3B.Q4_K_M.gguf"
 app = App()
-app.load_llm()
+app.load_llm(DEFAULT_LLM)
 
 with gr.Blocks() as server:
     with gr.Row():
         with gr.Column():
             t2s_text = gr.Textbox(label="Text to Speech", value="サンプル文章です。日本語で出力されているか確認してください。",lines=2, scale=3, max_lines=3)
             output_folder = gr.Textbox(label="Output folder name", value="",lines=1, scale=3, max_lines=1)
-            top_k_slider =  gr.Slider(minimum=0, maximum=100, value=0, step=1, precision=0, label="Top k")
-            top_p_slider =  gr.Slider(minimum=0, maximum=1, value=0.95, step=0.01, precision=2, label="Top p")
-            temperature_slider =  gr.Slider(minimum=0, maximum=2, value=0.9, step=0.01, precision=1, label="Temperature")
             max_tokens_slider =  gr.Slider(minimum=128, maximum=4096, value=512, step=64, precision=0, label="Max Tokens (trained with 2048)")
-            repeat_penalty_slider = gr.Slider(minimum=0, maximum=10, value=1.1, step=0.1, precision=1, label="Repeat Penalty")
-            llm_dropdown = gr.Dropdown([f[9:-5] for f in glob.glob("./models/*.gguf")], label="LLM", interactive=True)
-            system_prompt = gr.Textbox(label="System prompt", value="Convert the text to speech:", lines=2, max_lines=3, scale=3, interactive=True)
+            with gr.Accordion("LLM settings", open=False):
+                top_k_slider =  gr.Slider(minimum=0, maximum=100, value=0, step=1, precision=0, label="Top k")
+                top_p_slider =  gr.Slider(minimum=0, maximum=1, value=0.95, step=0.01, precision=2, label="Top p")
+                temperature_slider =  gr.Slider(minimum=0, maximum=2, value=0.9, step=0.01, precision=1, label="Temperature")
+                repeat_penalty_slider = gr.Slider(minimum=0, maximum=10, value=1.1, step=0.1, precision=1, label="Repeat Penalty")
+                llm_dropdown = gr.Dropdown([f[9:-5] for f in glob.glob("./models/*.gguf")], label="LLM", interactive=True)
+                system_prompt = gr.Textbox(label="System prompt", value="Convert the text to speech:", lines=2, max_lines=3, scale=3, interactive=True)
             user_audio = gr.Audio(label="Upload an audio file you want to continue generating", interactive=True, type="filepath", max_length=300)
             
         with gr.Column():
@@ -30,8 +32,5 @@ with gr.Blocks() as server:
     gen_button.click(fn=t2s_gen, inputs=[t2s_text, system_prompt, max_tokens_slider, top_k_slider, top_p_slider, temperature_slider, repeat_penalty_slider, output_folder, user_audio], outputs=audio_output)
 
     llm_dropdown.change(fn=lambda model_name: app.load_llm(f"./models/{model_name}.gguf"), inputs=llm_dropdown, outputs=None)
-
-    def set_persistent(is_persistent):
-        app.set_persistent(is_persistent)
-    is_persistent_check.change(fn=set_persistent, inputs=[is_persistent_check], outputs=None)
+    is_persistent_check.change(fn=lambda x: app.set_persistent(x), inputs=[is_persistent_check], outputs=None)
 server.launch(inbrowser=True)
